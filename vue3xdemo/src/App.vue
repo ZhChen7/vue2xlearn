6fc9e5acdc8c79b9991155e72de9ac7093abc4fa
zhouchen <1583741285@qq.com>
2020-09-22 09:46:38 +0800

    feat(add):init

---
+++ vue3xdemo/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1 @click="handlNum">{{ state.count }}</h1>
+    <h2>{{ double }}</h2>
+    <input type="text" v-model="state.message" />
+    <p>{{ state.message }}</p>
+    <div id="v-model-select-dynamic" class="demo">
+      <select v-model="state.selected">
+        <option
+          v-for="option in state.options"
+          :value="option.value"
+          :key="option"
+        >
+          {{ option.text }}
+        </option>
+      </select>
+      <span>Selected: {{ state.selected }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, onMounted } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 1,
+      message: "111",
+      selected: "A",
+      options: [
+        { text: "One", value: "A" },
+        { text: "Two", value: "B" },
+        { text: "Three", value: "C" },
+      ],
+    });
+
+    onMounted(() => {
+      console.log("component is mounted!");
+    });
+
+    const double = computed(() => state.count * 2);
+
+    function handlNum() {
+      state.count++;
+    }
+
+    return {
+      state,
+      handlNum,
+      double,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ vue3xdemo/src/App1.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- <button @click="increment">Count is: {{ state.count }}</button> -->
+  <h1>{{ count }}</h1>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 20,
+      msg: "hello world",
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
